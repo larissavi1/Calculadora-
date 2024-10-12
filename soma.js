d1afaf38f0f34d540908dd78a58a22d5f2c7d541
unknown <you@example.com>
2024-10-11 22:32:56 -0300

    primeiro commit

---
+++ soma.js
@@ -0,0 +1,12 @@
+function soma(numero1, numero2) {
+    return numero1 + numero2
+}
+
+module.exports = soma
+
+//export {soma}
+
+//export const usuario = {
+   // nome: 'Guido',
+   // email: 'guido@email.com'
+//}
